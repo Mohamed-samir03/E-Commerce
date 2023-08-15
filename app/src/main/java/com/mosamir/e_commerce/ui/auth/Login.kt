5b8995852f7517e1feea5c24583cebdab2828a68
mo samir <smyr03130@gmail.com>
2023-08-15 18:22:26 +0300

    add auth nav and finished login design

---
+++ app/src/main/java/com/mosamir/e_commerce/ui/auth/Login.kt
@@ -0,0 +1,42 @@
+package com.mosamir.e_commerce.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.mosamir.e_commerce.R
+import com.mosamir.e_commerce.databinding.FragmentLoginBinding
+
+
+class Login : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mNavController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mNavController = findNavController()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/mosamir/e_commerce/ui/auth/Register.kt
@@ -0,0 +1,41 @@
+package com.mosamir.e_commerce.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.mosamir.e_commerce.databinding.FragmentRegisterBinding
+
+
+class Register : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mNavController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mNavController = findNavController()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
